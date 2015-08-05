33a261aa237e77119841e47c307c740b05381f9e
yu-i9 <allgemeiner.pc@gmail.com>
2015-08-05 11:55:36 +0900

    de Bruijin レベルを使用するように修正した

--- Parser.hs
+++ Parser.hs
@@ -9,11 +9,35 @@ import qualified Text.Parsec.Token as P
 import           Text.Parsec.Language
 import           Data.Char
 
+{- --------------------------------------------------------
+               インデックスを付けるための機構
+   -------------------------------------------------------- -}
+
 data Context = Ctx {
       depth   :: Int,
       binding :: [(Char, Int)]
     }
 
+updateCtx :: Context -> Char -> Context
+updateCtx (Ctx d b) c = (Ctx (d+1) ((c, d):b))
+
+freeVar :: Char -> Int
+freeVar c = (ord c) - (ord 'a')
+
+{- 外側から小さい値を使う de Bruijin レベルを使う．
+   束縛変数ならばそのまま値を返してよく，
+   自由変数ならば深さの分だけシフトしてインデックスとする．-}
+index :: Context -> Char -> Int
+index (Ctx depth bind) v =
+    case lookup v bind of
+      Just d  -> d
+      Nothing -> depth + freeVar v
+
+
+
+{- --------------------------------------------------------
+                          パーサー
+   -------------------------------------------------------- -}
 parseTerm :: String -> Check Term
 parseTerm input =
     case parse (term $ Ctx 0 []) "" input of
@@ -97,15 +121,3 @@ termif c = do
   t <- try (symbol "then") >> termBody c
   f <- try (symbol "else") >> termBody c
   return $ TmIf p t f
-
-updateCtx :: Context -> Char -> Context
-updateCtx (Ctx d b) c = (Ctx (d+1) ((c, d):b))
-
-freeVar :: Char -> Int
-freeVar c = (ord c) - (ord 'a')
-
-index :: Context -> Char -> Int
-index (Ctx depth bind) v =
-    case lookup v bind of
-      Just d  -> depth - d - 1
-      Nothing -> depth + freeVar v
